b3d203170b9d82b97a54178ad1b62dc7ab7f959a
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-04-19 17:40:51 +0200

    fixed imports and changed exception handler class name for better code readability

--- src/main/java/kaem0n/u5w3d5/exceptions/ExceptionManager.java
+++ src/main/java/kaem0n/u5w3d5/exceptions/ExceptionManager.java
@@ -5,14 +5,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.security.access.AccessDeniedException;
 
-import java.nio.file.AccessDeniedException;
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
-public class ExceptionHandler {
-    @org.springframework.web.bind.annotation.ExceptionHandler(BadRequestException.class)
+public class ExceptionManager {
+    @ExceptionHandler(BadRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponseDTO handleBadRequest(BadRequestException e) {
         if (e.getErrorList() != null) {
@@ -23,34 +24,34 @@ public class ExceptionHandler {
         else return new ErrorResponseDTO(e.getMessage(), LocalDateTime.now());
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundException.class)
+    @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponseDTO handleNotFound(NotFoundException e) {
         return new ErrorResponseDTO(e.getMessage(), LocalDateTime.now());
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+    @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponseDTO handleGenericError(Exception e) {
         e.printStackTrace();
         return new ErrorResponseDTO("Internal server error.", LocalDateTime.now());
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(MissingServletRequestParameterException.class)
+    @ExceptionHandler(MissingServletRequestParameterException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponseDTO handleMissingRequestParameter(MissingServletRequestParameterException e) {
         return new ErrorResponseDTO(e.getMessage() + ".", LocalDateTime.now());
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(UnauthorizedException.class)
+    @ExceptionHandler(UnauthorizedException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public ErrorResponseDTO handleUnauthorized(UnauthorizedException e) {
         return new ErrorResponseDTO(e.getMessage(), LocalDateTime.now());
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(org.springframework.security.access.AccessDeniedException.class)
+    @ExceptionHandler(AccessDeniedException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ErrorResponseDTO handleUnauthorized(org.springframework.security.access.AccessDeniedException e) {
+    public ErrorResponseDTO handleUnauthorized(AccessDeniedException e) {
         return new ErrorResponseDTO(e.getMessage(), LocalDateTime.now());
     }
 }

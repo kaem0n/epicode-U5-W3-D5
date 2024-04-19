11b7dada8590f3b87ce33e9e99b22589cc33d482
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-04-19 11:01:52 +0200

    adds organizer attribute in event class + db relation

--- src/main/java/kaem0n/u5w3d5/entities/Event.java
+++ src/main/java/kaem0n/u5w3d5/entities/Event.java
@@ -23,17 +23,21 @@ public class Event {
     private String title;
     private String description;
     private LocalDate date;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User organizer;
     private String place;
     @Column(name = "max_capacity")
     private int maxCapacity;
     @OneToMany(mappedBy = "event")
     private List<Ticket> tickets;
 
-    public Event(String title, String description, String place, LocalDate date, int maxCapacity) {
+    public Event(String title, String description, String place, LocalDate date, int maxCapacity, User organizer) {
         this.title = title;
         this.description = description;
         this.place = place;
         this.date = date;
         this.maxCapacity = maxCapacity;
+        this.organizer = organizer;
     }
 }

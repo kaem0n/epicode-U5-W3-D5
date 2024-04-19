a9916085f360bae4a32075bff65c6f826182036f
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-04-19 11:57:27 +0200

    adds user controller + payload validation

---
+++ src/main/java/kaem0n/u5w3d5/controllers/UserController.java
@@ -0,0 +1,50 @@
+package kaem0n.u5w3d5.controllers;
+
+import kaem0n.u5w3d5.entities.User;
+import kaem0n.u5w3d5.exceptions.BadRequestException;
+import kaem0n.u5w3d5.payloads.UserDTO;
+import kaem0n.u5w3d5.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService us;
+
+    @GetMapping
+    private Page<User> getAllUsers(@RequestParam(defaultValue = "0") int page,
+                                       @RequestParam(defaultValue = "10") int size,
+                                       @RequestParam(defaultValue = "id") String sort) {
+        return us.findAll(page, size, sort);
+    }
+
+    @GetMapping("/{id}")
+    private User getUserById(@PathVariable long id) {
+        return us.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    private void deleteUser(@PathVariable long id) {
+        us.delete(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    private User saveNewUser(@RequestBody @Validated UserDTO payload, BindingResult validation) {
+        if (validation.hasErrors()) throw new BadRequestException(validation.getAllErrors());
+        else return us.save(payload);
+    }
+
+    @PutMapping("/{id}")
+    private User updateUser(@PathVariable long id, @RequestBody @Validated UserDTO payload, BindingResult validation) {
+        if (validation.hasErrors()) throw new BadRequestException(validation.getAllErrors());
+        else return us.update(id, payload);
+    }
+}
--- src/main/java/kaem0n/u5w3d5/payloads/UserDTO.java
+++ src/main/java/kaem0n/u5w3d5/payloads/UserDTO.java
@@ -1,8 +1,23 @@
 package kaem0n.u5w3d5.payloads;
 
-public record UserDTO(String username,
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record UserDTO(@NotEmpty(message = "Username field must not be empty.")
+                      @Size(min = 3, max = 20, message = "Username field length must be between 3 and 20 characters.")
+                      String username,
+                      @NotEmpty(message = "Password field must not be empty.")
+                      @Size(min = 8, message = "Password must be at least 8 characters long.")
                       String password,
+                      @NotEmpty(message = "Full name field must not be empty.")
+                      @Size(min = 3, max = 20, message = "Full name field length must be between 3 and 20 characters.")
                       String fullName,
+                      @NotEmpty(message = "Email field must not be empty.")
+                      @Email(message = "Invalid email format.")
                       String email,
+                      @NotEmpty(message = "Role field must not be empty.")
+                      @Pattern(regexp = "USER|EVENT_ORGANIZER", message = "Invalid role value (available: USER, EVENT_ORGANIZER")
                       String role) {
 }

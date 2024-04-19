37e9c01b8af76d6e73840786f2f90c4210b9de91
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-04-19 10:13:56 +0200

    Initial commit

---
+++ src/test/java/kaem0n/u5w3d5/U5W3D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package kaem0n.u5w3d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

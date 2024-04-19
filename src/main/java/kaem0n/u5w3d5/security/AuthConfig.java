05a9fa19dd03a9fa3016a408efb8ea67e22bbae2
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-04-19 14:22:18 +0200

    adds auth configuration class

---
+++ src/main/java/kaem0n/u5w3d5/security/AuthConfig.java
@@ -0,0 +1,29 @@
+package kaem0n.u5w3d5.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AuthConfig {
+    @Bean
+    SecurityFilterChain sfc(HttpSecurity hs) throws Exception {
+        hs.formLogin(http -> http.disable());
+        hs.csrf(http -> http.disable());
+        hs.sessionManagement(http -> http.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        hs.authorizeHttpRequests(http -> http.requestMatchers("/**").permitAll());
+
+        return hs.build();
+    }
+
+    @Bean
+    PasswordEncoder getBCrypt() {
+        return new BCryptPasswordEncoder(13);
+    }
+}
